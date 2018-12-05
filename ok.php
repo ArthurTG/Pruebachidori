9ad04e1fa15b8b883235ef93ce2f1b398f97085c
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-05 16:32:32 -0600

    Update ok.php

--- ok.php
+++ ok.php
@@ -9,7 +9,7 @@ $conexion = pg_connect(
   " password=" . $datos["pass"]);
 
 // preparar consultas
-pg_prepare($conexion, "sql4", 'SELECT * FROM datosunity');
+pg_prepare($conexion, "sql4", 'SELECT * FROM XerathDatos');
 // ejecutar consultas
 $resultado = pg_execute($conexion, "sql4", array());
 // indicar que el resultado es JSON
