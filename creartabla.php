2a3f3a177340284272d2e5e935d31535c1645082
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-05 14:35:29 -0600

    Update creartabla.php

--- creartabla.php
+++ creartabla.php
@@ -21,7 +21,7 @@ function get1xd($numeritos) {
 $asdfg = getxd("insertar");
 $asddsa = get1xd("numeritos");
 // preparar consultas
-pg_prepare($conexion, "sql1", 'DROP TABLE IF EXISTS XerathDatos');
+pg_prepare($conexion, "sql1", 'DROP TABLE IF EXISTS datosunity');
 pg_prepare($conexion, "sql2", 'CREATE TABLE XerathDatos (nombre VARCHAR(30), numero INT)');
 pg_prepare($conexion, "sql3", 'INSERT INTO XerathDatos (nombre, numero) VALUES ($1, $2)');
 pg_prepare($conexion, "sql4", 'SELECT * FROM XerathDatos');
