202ebd2fdd5b768a7c1c0d21c5d176fac21c79af
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-05 19:01:28 -0600

    Update borrar.php

--- borrar.php
+++ borrar.php
@@ -8,11 +8,11 @@ $conexion = pg_connect(
   " user=" . $datos["user"] . 
   " password=" . $datos["pass"]);
 // preparar consultas
-pg_prepare($conexion, "sql1", 'DROP TABLE IF EXISTS XerathDatos');
-pg_prepare($conexion, "sql2", 'DROP TABLE IF EXISTS XerathDatosxd');
+pg_prepare($conexion, "borrar1", 'DROP TABLE IF EXISTS XerathDatos');
+pg_prepare($conexion, "borrar2", 'DROP TABLE IF EXISTS XerathDatosxd');
 // ejecutar consultas
-pg_execute($conexion, "sql1", array());
-pg_execute($conexion, "sql2", array());
+pg_execute($conexion, "borrar1", array());
+pg_execute($conexion, "borrar2", array());
 // indicar que el resultado es JSON
 header("Content-type: application/json; charset=utf-8");
 // permitir acceso de otros lugares fuera del servidor
