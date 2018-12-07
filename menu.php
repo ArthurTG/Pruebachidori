6a9829cc1890b6c3b7f33bff0b1629f6e431533c
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-06 18:46:51 -0600

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,18 @@
+<?php include 'constantes.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Usuarios Conectados</title>
+        <?php include 'html/estilos.html'; ?>
+    </head>
+    <body>
+        <div class="container">
+            <br/>
+            <h1>Usuarios Conectados</h1>
+            <hr class="my-4">
+            <div class="btn-group-vertical" id="conectados">
+            </div>
+        </div>
+        <script src="ajax.js"></script>
+    </body>
+</html>
