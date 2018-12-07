f0c6a1e861ab4b7a90a1718f760b042cec7c593c
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-07 16:06:48 -0600

    Update Página.php

--- Página.php
+++ Página.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Hola mundo</title>
+	<title>askdfjadf</title>
 </head>
 <body>
-este es un ejemplo de heroku
+	<table border="1">
+		<tr>
+			<td>Asesino</td>
+			<td>Coordenada X</td>
+			<td>Coordenada Y</td>
+			<td>Coordenada Z</td>
+		</tr>
+		<tr>
+			<td>minion</td>
+			<td>12</td>
+			<td>34</td>
+			<td>57</td>
+		</tr>
+	</table>
 </body>
-</html>	
+</html>
