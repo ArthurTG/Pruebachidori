bc3a19c7779b18d0fa2802d0d5c4e7eb7d6a5c03
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-06 20:21:03 -0600

    Update insertardatos.php

--- insertardatos.php
+++ insertardatos.php
@@ -18,26 +18,15 @@ function getposX($posX) {
   return $variablita;
 }
 
-function getposY($posY) {
-  $variablitaY = json_decode($_GET[$posY]);
-  return $variablitaY;
-}
-
-function getposZ($posZ) {
-  $variablitaZ = json_decode($_GET[$posZ]);
-  return $variablitaZ;
-}
 
 $asdfg = getase("asesino");
 $asddsa = getposX("posX");
-$asddsay = getposY("posY");
-$asddsaz = getposZ("posZ");
 
 // preparar consultas
-pg_prepare($conexion, "insertar1", 'INSERT INTO XerathDatos (asesino, x, y, z) VALUES ($1, $2, $3, $4)');
+pg_prepare($conexion, "insertar1", 'INSERT INTO XerathDatos (asesino, x) VALUES ($1, $2)');
 pg_prepare($conexion, "insertar2", 'SELECT * FROM XerathDatos');
 // ejecutar consultas
-pg_execute($conexion, "insertar1", array("$asdfg", "$asddsa", "$asddsay", "$asddsaz"));
+pg_execute($conexion, "insertar1", array("$asdfg", "$asddsa"));
 $resultado = pg_execute($conexion, "insertar2", array());
 // indicar que el resultado es JSON
 header("Content-type: application/json; charset=utf-8");
