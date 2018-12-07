3a25d8f810a09b2a16381cd164a06bdfbefe338f
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-07 16:23:17 -0600

    Update Página.php

--- Página.php
+++ Página.php
@@ -1,3 +1,15 @@
+<?
+$datos = parse_url($_ENV["DATABASE_URL"]);
+// conectarse
+$conexion = pg_connect(
+  "host=" . $datos["host"] . 
+  " port=" . $datos["port"] . 
+  " dbname=" . substr($datos["path"], 1) . 
+  " user=" . $datos["user"] . 
+  " password=" . $datos["pass"]);
+
+?>
+	
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,12 +23,31 @@
 			<td>Coordenada Y</td>
 			<td>Coordenada Z</td>
 		</tr>
-		<tr>
-			<td>minion</td>
-			<td>12</td>
-			<td>34</td>
-			<td>57</td>
-		</tr>
+		
+		<?php
+		// preparar consultas
+		pg_prepare($conexion, "sql4", 'SELECT * FROM XerathDatos');
+		// ejecutar consultas
+		$resultado = pg_execute($conexion, "sql4");
+		
+		// indicar que el resultado es JSON
+		header("Content-type: application/json; charset=utf-8");
+		// permitir acceso de otros lugares fuera del servidor
+		header('Access-Control-Allow-Origin: *');
+		
+		while($fila = pg_fetch_assoc($resultado))
+		{		
+			?>
+			<tr>
+				<td><?php echo $fila['asesino']?></td>
+				<td><?php echo $fila['X']?></td>
+				<td><?php echo $fila['Y']?></td>
+				<td><?php echo $fila['Z']?></td>
+			</tr>
+
+			<?php
+		}
+		?>
 	</table>
 </body>
 </html>
