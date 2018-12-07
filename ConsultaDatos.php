7dd549bacdbc6c22615002f30470382e605ee993
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-07 17:51:09 -0600

    Create ConsultaDatos.php

---
+++ ConsultaDatos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>askdfjadf</title>
+</head>
+<body>
+	<table border="1">
+		<tr>
+			<td>Asesino</td>
+			<td>Coordenada X</td>
+			<td>Coordenada Y</td>
+			<td>Coordenada Z</td>
+		</tr>
+		<tr>
+			<td>minion</td>
+			<td>12</td>
+			<td>34</td>
+			<td>57</td>
+		</tr>
+	</table>
+</body>
+</html>
