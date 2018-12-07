bd5ed60f311ecc25aed0c7f94d1351ffb85f8a81
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-07 17:50:03 -0600

    Update Página.php

--- Página.php
+++ Página.php
@@ -26,28 +26,13 @@ while ($fila = pg_fetch_assoc($resultado))
   array_push($gente, $fila);
   
   ?>
-
-  <!DOCTYPE html>
+	
 <html>
-<head>
-	<title>askdfjadf</title>
-</head>
-<body>
-	<table border="1">
-		<tr>
-			<td>Asesino</td>
-			<td>Coordenada X</td>
-			<td>Coordenada Y</td>
-			<td>Coordenada Z</td>
-		</tr>
-		<tr>
-			<td>minion</td>
-			<td>12</td>
-			<td>34</td>
-			<td>57</td>
-		</tr>
-	</table>
-</body>
+	<head>
+	</head>
+	<body>
+		<h1>hola</h1>
+	</body>	
 </html>
 
   <?php
