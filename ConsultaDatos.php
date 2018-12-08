04ff3f0adfa4f2a3729644b33ebd2d5bfa7a2b88
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-08 04:13:54 -0600

    Update ConsultaDatos.php

--- ConsultaDatos.php
+++ ConsultaDatos.php
@@ -33,12 +33,12 @@ $conexion = pg_connect(
 			<td>Puntuación</td>
 		</tr>		
 		<?
-			pg_prepare($conexion, "sql3", 'SELECT * FROM XerathDatos');
+			pg_prepare($conexion, "sql3", 'SELECT * FROM XerathDatosxd');
 			$resultado = pg_execute($conexion, "sql3", array());
 			header('Access-Control-Allow-Origin: *');
 			
 			$usuarios = array();
-			while ($fila1 = pg_fetch_assoc($resultado1)) 
+			while ($fila1 = pg_fetch_assoc($resultado)) 
 			{
 			  array_push($usuarios, $fila1);		
 		?>		
@@ -52,6 +52,32 @@ $conexion = pg_connect(
 		?>		
 	</table>
 		
+	<table id="tabla1" border="1">
+		<tr>
+			<td>Asesino</td>
+			<td>Coordenada X</td>
+			<td>Coordenada Y</td>
+			<td>Coordenada Z</td>
+		</tr>		
+		<?
+			pg_prepare($conexion, "sql4", 'SELECT * FROM XerathDatos');
+			$resultado1 = pg_execute($conexion, "sql4", array());
+			header('Access-Control-Allow-Origin: *');
+			
+			$gente = array();
+			while ($fila = pg_fetch_assoc($resultado1)) 
+			{
+			  array_push($gente, $fila);
+		?>
+			<td id="tabla"><? echo $fila['asesino']; ?></td>
+			<td id="tabla"><? echo $fila['x']; ?></td>
+			<td id="tabla"><? echo $fila['y']; ?></td>
+			<td id="tabla"><? echo $fila['z']; ?></td>
+		</tr>
+		<?
+			}
+		?>	
+	</table>
 	</center>
 </body>
 </html>
