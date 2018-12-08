d180b044b265be262d971ca6a9005c5dc6cdd7ff
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-07 18:03:37 -0600

    Update Página.php

--- Página.php
+++ Página.php
@@ -6,7 +6,25 @@ $conexion = pg_connect(
   " port=" . $datos["port"] . 
   " dbname=" . substr($datos["path"], 1) . 
   " user=" . $datos["user"] . 
-  " password=" . $datos["pass"]);  
+  " password=" . $datos["pass"]);
+
+// preparar consultas
+pg_prepare($conexion, "sql3", 'SELECT * FROM XerathDatos');
+pg_prepare($conexion, "sql4", 'SELECT * FROM XerathDatosxd');
+// ejecutar consultas
+$resultado = pg_execute($conexion, "sql3", array());
+$resultado1 = pg_execute($conexion, "sql4", array());
+// indicar que el resultado es JSON
+header("Content-type: application/json; charset=utf-8");
+// permitir acceso de otros lugares fuera del servidor
+header('Access-Control-Allow-Origin: *');
+
+// imprimir resultado
+$gente = array();
+while ($fila = pg_fetch_assoc($resultado)) 
+{
+  array_push($gente, $fila);
+  
   ?>
 	
 <html>
