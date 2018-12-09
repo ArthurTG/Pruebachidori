c2be6705fce2cbd8a2eb6586e6db8c676545d145
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2018-12-09 04:35:42 -0600

    Update ConsultaDatos.php

--- ConsultaDatos.php
+++ ConsultaDatos.php
@@ -54,9 +54,9 @@ $conexion = pg_connect(
 			
 			
 			$usuarios = array();
-			while ($fila1 = pg_fetch_assoc($resultado) && $fila = pg_fetch_assoc($resultado1)) 
+			while ($fila1 = pg_fetch_assoc($resultado)) 
 			{
-			  	array_push($usuarios, $fila1, $fila);	
+			  	array_push($usuarios, $fila1);	
 		?>			
 		<tr>
 				<td id="tabla"><? echo $fila1['usuario']?> </td>
